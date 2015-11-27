7ca635e21a586123160f25a1d79d109f14497694
Jan Wolski <jan@wolski.fi>
2015-11-27 18:22:18 +0200

    Added headings & sorting interactions

--- src/getmovies.js
+++ src/getmovies.js
@@ -4,15 +4,19 @@ module.exports = function getMovies(callback){
 	//$.getJSON("https://spreadsheets.google.com/feeds/list/16xOjTfqfETXF5Avppl4d-AjJOkDQKuHFdib6ZpHXmQQ/1/public/values?alt=json").done(parse);
 	$.getJSON("test_data.json").done(parse);
 	function parse(data) {
-		var meat = data.feed.entry.map(function(e, i) {
+		var meat = {};
+		meat.headings = [
+			'valmistusvuosi',
+			'nimi',
+			'synopsis',
+			'tekijät'
+		];
+		meat.table = data.feed.entry.map(function(e, i) {
 			return {
 				id: i,
-				cells: [
-					e.gsx$valmistusvuosi.$t,
-					e.gsx$nimi.$t,
-					e.gsx$synopsis.$t,
-					e.gsx$tekijät.$t
-				]
+				cells: meat.headings.map(function(heading) {
+					return e['gsx$' + heading].$t
+				})
 			};
 		});
 		callback(meat);
--- src/movietable.jsx
+++ src/movietable.jsx
@@ -3,8 +3,30 @@
 var React = require('react')
 module.exports = React.createClass({
     displayName: 'MovieTable',
+    getInitialState: function(){
+        return {
+            sortColumn: 0,
+            sortAscending : true
+        };
+    },
+    handleClick: function(SynthEvent, id){
+        var col = parseInt(id.slice(-1));
+        this.setState({
+            sortColumn: col,
+            sortAscending: this.state.sortColumn === col ? !this.state.sortAscending : this.state.sortAscending
+        });
+    },
     render: function(){
-        var rows = this.props.data.map(function(row) {
+        var sorted = this.props.data.table.slice().sort( (function(a,b){
+            var c = this.state.sortColumn;
+            return this.state.sortAscending ? a.cells[c].localeCompare(b.cells[c]) : b.cells[c].localeCompare(a.cells[c]);
+        }).bind(this));
+        var headings = this.props.data.headings.map(function(heading) {
+            return (
+                <th onClick={this.handleClick}>{heading}</th>
+            )
+        }, this);
+        var rows = sorted.map(function(row) {
             var cells = row.cells.map(function(cell){
                 return (
                     <td>{cell}</td>
@@ -18,6 +40,9 @@ module.exports = React.createClass({
         });
         return (
             <table>
+                <thead>
+                <tr>{headings}</tr>
+                </thead>
                 <tbody>
                 {rows}
                 </tbody>
