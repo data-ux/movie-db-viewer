165eb9970e248f1470b570f91e31a62d3a270751
Jan Wolski <jan@wolski.fi>
2015-12-03 17:09:08 +0200

    bundle path fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     entry: './src/code/index.jsx',
     output: {
-        filename: 'dist/bundle.js', //this is the default name, so you can skip it
+        filename: 'bundle.js', //this is the default name, so you can skip it
         //at this directory our bundle file will be available
         //make sure port 8090 is used when launching webpack-dev-server
-        publicPath: 'http://localhost:8090/dist'
+        publicPath: 'http://localhost:8090/assets'
     },
     module: {
         loaders: [
