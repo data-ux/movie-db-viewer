ecab8f9d843e5b3dd2ccb174b2d79fb6e48e1727
Jan Wolski <jan@wolski.fi>
2016-02-19 15:11:18 +0200

    Added search with quoted phrase

--- src/code/movieui.jsx
+++ src/code/movieui.jsx
@@ -33,7 +33,20 @@ module.exports = React.createClass({
     handleSearch: function(searchOptions){
         var self = this;
         var vimeoColumn = this.state.movies.headings.indexOf('vimeo');
-        var terms = searchOptions.searchTerm.toLowerCase().split(' ').filter(function(t){return t.length > 1});
+        
+        var terms = [];
+        var phraseMatcher = /[“”"]([^“”"]+)[““"$]?/g;
+        var searchTermStr = searchOptions.searchTerm.toLowerCase();
+        var match;
+        while(match = phraseMatcher.exec(searchTermStr)){
+            terms.push(match[1]);
+        }
+        if(terms.length > 0){
+            searchTermStr = searchTermStr.replace(/[“”"][^“”"]+[“”"$]?/g, '');
+        }
+        var words = searchTermStr.split(/\s+/).filter(function(t){return t.length > 1});
+        terms = terms.concat(words);
+        
         var filteredTable = this.state.movies.table.filter(function(e){
             return (searchOptions.checkBox ? e.cells[vimeoColumn] !== '' : true) && terms.every(function(term){ return e.cells.some(function(c, index){
                     if(c === '') return false;
